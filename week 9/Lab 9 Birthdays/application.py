fa435732a1e33bcf1c39e3f28954a1e91a434233
ToanTran <tranquoctoan.ce@gmail.com>
2022-04-04 16:06:19 +0700

    Update some exercises of week 0 and week 9!

---
+++ week 9/Lab 9 Birthdays/application.py
@@ -0,0 +1,37 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///birthdays.db")
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+
+        # TODO: Add the user's entry into the database
+        message = ""
+        name = request.form.get("name")
+        month = request.form.get("month")
+        day = request.form.get("day")
+        if not name or not month or not day:
+            message = "Missing input"
+        else:
+            db.execute("INSERT INTO birthdays(name, month, day) VALUES(?,?,?)", name, month, day)
+            birthdays = db.execute("SELECT * FROM birthdays")
+            return render_template("index.html", birthdays = birthdays, message = message)
+        return redirect("/")
+
+    else:
+        # TODO: Display the entries in the database on index.html
+        birthdays = db.execute("SELECT * FROM birthdays")
+        return render_template("index.html", birthdays = birthdays)
+
+
